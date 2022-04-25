e80f8984449fe32ed67922e3ad84152ca59a6006
AnaCarolina12 <76164929+AnaCarolina12@users.noreply.github.com>
2022-04-25 11:01:41 +0200

    Delete Empleados directory

--- Empleados/src/main/java/com/example/Empleados/exceptions/NoContentException.java
+++
@@ -1,8 +0,0 @@
-package com.example.Empleados.exceptions;
-
-public class NoContentException extends  RuntimeException{
-
-    public NoContentException(String message){
-        super(message);
-    }
-}
