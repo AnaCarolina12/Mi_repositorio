9cc3cbc145a09f4cb242f73065de89d30022c7fb
AnaCarolina12 <carolinacucki5@gmail.com>
2022-04-28 11:28:12 +0200

    Update

---
+++ Empleados/src/test/java/com/example/Empleados/controller/EmpleadosControllerTest.java
@@ -0,0 +1,26 @@
+package com.example.Empleados.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmpleadosControllerTest {
+
+
+
+    @Test
+    void getAllEmpleados() {
+    }
+
+    @Test
+    void getEmpleado() {
+    }
+
+    @Test
+    void addUpdateEmpleados() {
+    }
+
+    @Test
+    void cleanEmpelados() {
+    }
+}
