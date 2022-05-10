dcead7ef071fcf9d6aff5a4669d547732d8b9bb0
AnaCarolina12 <carolinacucki5@gmail.com>
2022-05-10 11:29:33 +0200

    Nuevos

--- Empleados/src/test/java/com/example/Empleados/exceptions/HandleExceptionsTest.java
+++ Empleados/src/test/java/com/example/Empleados/exceptions/HandleExceptionsTest.java
@@ -5,12 +5,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.HttpStatus;
 
-@AutoConfigureMockMvc
 class HandleExceptionsTest {
-  
+
   @BeforeEach
   void setUp() {
     MockitoAnnotations.openMocks(this);
