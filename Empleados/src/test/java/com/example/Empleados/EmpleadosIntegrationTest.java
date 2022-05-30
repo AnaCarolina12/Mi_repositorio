2d62aae8b1faacb074d4e180b2c7441f56ac9ce9
AnaCarolina12 <carolinacucki5@gmail.com>
2022-05-30 15:52:27 +0200

    Nuevos

---
+++ Empleados/src/test/java/com/example/Empleados/EmpleadosIntegrationTest.java
@@ -0,0 +1,73 @@
+package com.example.Empleados;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.example.Empleados.model.Empleados;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.Test;
+
+class EmpleadosIntegrationTest {
+
+  @Test
+  void getAllEmpleados() {
+
+    // quiero verificar que el cuerpo del objeto JSON no esté vacio.
+    ValidatableResponse response =
+        given().contentType(ContentType.JSON).when().get("/empleados").then()
+            .body("empty", equalTo(false))
+            .statusCode(200);
+
+    //.... .log().body();
+
+  }
+
+  @Test
+  void getEmpleado() {
+
+    ValidatableResponse response = given().contentType(ContentType.JSON).
+        pathParameters("dni", "11111112T").when().get("/empleados/{dni}")
+        .then().statusCode(200);
+
+    Empleados e = response.extract().as(Empleados.class);
+
+    assertEquals("11111112T", e.getDni());
+    assertEquals("AnaContainer", e.getNombre());
+    assertEquals("Sangucho", e.getApellidos());
+  }
+
+  @Test
+  void addUpdateEmpleados() {
+    Empleados empleados1 = getEmpleadosModel("12345678M", "Ana", "Cruz");
+    ValidatableResponse response = given().contentType(ContentType.JSON)
+        .body(empleados1).when().post("/empleados").then().statusCode(200);
+
+    Empleados e = response.extract().as(Empleados.class);
+
+    assertEquals("12345678M", e.getDni());
+    assertEquals("Ana", e.getNombre());
+    assertEquals("Cruz", e.getApellidos());
+  }
+
+  @Test
+  void deleteEmpleados() {
+
+    ValidatableResponse response =
+        given().contentType(ContentType.JSON).pathParameters("dni", "12345678M").when().delete("/empleados/{dni}")
+            .then().statusCode(200);
+
+  }
+
+  private Empleados getEmpleadosModel(String dni, String nombre, String apellidos) {
+    Empleados empleados = new Empleados();
+
+    empleados.setDni(dni);
+    empleados.setNombre(nombre);
+    empleados.setApellidos(apellidos);
+
+    return empleados;
+  }
+
+}
---
+++ Empleados/src/test/java/com/example/Empleados/service/EmpleadosServiceImpIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.example.Empleados.service;
+
+import org.junit.jupiter.api.Test;
+
+class EmpleadosServiceImpIntegrationTest {
+
+  @Test
+  void getAllEmpleados() {
+
+  }
+
+  @Test
+  void getEmpleados() {
+
+  }
+
+  @Test
+  void addUpdateEmpleados() {
+
+  }
+
+  @Test
+  void deleteEmpleados() {
+
+  }
+
+
+}
